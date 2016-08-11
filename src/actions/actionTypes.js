4c0455455b6b93dec9e3d721ea264f4e4f21e4d0
ilwp <zhao_muyun@sina.cn>
2016-08-12 07:54:46 +0800

    redux新增注册功能。

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -5,13 +5,11 @@
 /* 首页相关actions */
 export const GET_TOTAL_AMOUNT = "GET_TOTAL_AMOUNT"; /* 获取总金额 */
 export const GET_LATELY_LIST = "GET_LATELY_LIST"; /* 获取最近订单列表 */
-
 export const GET_TOTAL_AMOUNT_SUCCESS = "GET_TOTAL_AMOUNT_SUCCESS"; /* 未结算金额查询完成 */
-
 export const GET_LATELY_LIST_SUCCESS = "GET_LATELY_LIST_SUCCESS"; /* 最近账单查询完成 */
 
 /* 登录相关actions */
 export const POST_LOGIN_SUCCESS = "POST_LOGIN_SUCCESS"; /* 登录成功 */
-export const POST_LOGIN_FAILURE = "POST_LOGIN_FAILURE"; /* 登录成功 */
-
-export const LOGIN_STATE = "LOGIN_STATE"; /* 登录状态 */
+export const POST_LOGIN_FAILURE = "POST_LOGIN_FAILURE"; /* 登录失败 */
+export const POST_REG_SUCCESS = "POST_REG_SUCCESS"; /* 注册成功 */
+export const POST_REG_FAILURE = "POST_REG_FAILURE"; /* 注册失败 */
--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -4,16 +4,30 @@
 import * as actionTypes from './actionTypes'; /* actionType */
 import ref from '../wilddog_ref';
 
+/* 根据错误码获取错误信息 */
+const getStateMsg = code => {
+	return {
+		'invalid_email': '邮箱验证失败！',
+		'invalid_password': '密码错误',
+		'email_taken': '该邮箱已被注册！'
+	}[code];
+}
+
+/*
+ *
+ * 登录
+ *
+ */
 /* 用户登录 */
 const userLogin = (email, password) => {
-	console.log(email);
-	console.log(password);
 	return dispatch => {
 		return ref.authWithPassword({
 			email,
 			password
 		}, (err, data) => {
-			err ? dispatch(loginError(err)) : dispatch(loginSuccess(data));
+			err ? 
+			dispatch(loginError(err)) : 
+			dispatch(loginSuccess(data));
 		});
 	}
 }
@@ -25,9 +39,51 @@ const loginSuccess = userData => ({
 });
 
 /* 登录失败 */
-const loginError = loginError => ({
-	type: actionTypes.POST_LOGIN_FAILURE,
-	loginError
+const loginError = loginError => {
+	let code = loginError.code;
+
+	/* 判断是否为email验证失败 */
+	let errType = code.indexOf('email') > 0;
+
+	return {
+		type: actionTypes.POST_LOGIN_FAILURE,
+		emailErrMsg: errType && getStateMsg(code),
+		pwdErrMsg: errType || getStateMsg(code),
+		showUnameErr: errType,
+		showPwdErr: !errType
+	}
+};
+
+/*
+ *
+ * 注册
+ *
+ */
+/* 创建用户 */
+const userReg = (email, password) => {
+	return dispatch => {
+		return ref.createUser({
+			email,
+			password
+		}, (err, data) => {
+			err ? 
+			dispatch(regError(err)) : 
+			dispatch(regSuccess(data))
+		});
+	}
+}
+
+/* 注册成功 */
+const regSuccess = data => ({
+	type: actionTypes.POST_REG_SUCCESS
 });
 
-export { userLogin };
+/* 注册失败 */
+const regError = err => ({
+	type: actionTypes.POST_REG_FAILURE
+});
+
+export { 
+	userLogin, 
+	userReg 
+};
